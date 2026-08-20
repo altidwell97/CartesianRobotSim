9521b09f5e8955e9615473d4fa7fcf7841cafc73
Alex Tidwell <altidwell97@gmail.com>
2026-08-20 04:15:37 -0400

    Added UI Visual no functionality

---
+++ CartesianRobotSim/MVVM/View/MemorizedPositions.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CartesianRobotSim.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for MemorizedPositions.xaml
+    /// </summary>
+    public partial class MemorizedPositions : UserControl
+    {
+        public MemorizedPositions()
+        {
+            InitializeComponent();
+        }
+    }
+}
